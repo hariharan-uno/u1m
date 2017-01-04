adec2487cdd793b03815e1fd31f38cf11d3381b9
Kris Foster <kris.foster@gmail.com>
2017-01-03 21:16:14 -0800

    Add ranks to a history table and expose via /history endpoint (#2)
    * domain history table
    
    * separate queries from handlers
    
    * a really bad daily update for u1m, work in progress
    
    * Add the /history endpoint to serve up the history of a domain

--- api/api.go
+++ api/api.go
@@ -9,8 +9,17 @@ import (
 
 // Domain represents a domain in the database
 type Domain struct {
-	Domain string `json:"domain"`
+	Domain string `json:"domain,omitempty"`
 	Rank   int    `json:"rank"`
+	Day    string `json:"day,omitempty"`
+}
+
+// GetDomain returns details of a domain
+func (s *Server) GetDomain(domain string) (Domain, error) {
+	var d Domain
+	err := s.db.Get(&d, "SELECT domain, rank FROM current WHERE domain=?", domain)
+	return d, err
+
 }
 
 // GetDomainHandler returns the current details for a given domain
@@ -23,8 +32,9 @@ func (s *Server) GetDomainHandler() http.HandlerFunc {
 		vars := mux.Vars(r)
 		domain := vars["domain"]
 
-		var domainRes Domain
-		if err = s.db.Get(&domainRes, "SELECT domain, rank FROM current WHERE domain=?", domain); err != nil {
+		domainRes, err := s.GetDomain(domain)
+		if err != nil {
+			s.log.Errorln(err)
 			httpResponse(w, &errorResponse{Error: "Domain not found"}, http.StatusNotFound)
 			return
 		}
@@ -32,10 +42,51 @@ func (s *Server) GetDomainHandler() http.HandlerFunc {
 	})
 }
 
+// History represents the known ranks of a domain
+type History struct {
+	Domain  string   `json:"domain"`
+	History []Domain `json:"history"`
+}
+
+// GetHistory returns history of a domain's ranks
+func (s *Server) GetHistory(domain string) (History, error) {
+	var h History
+	h.Domain = domain
+	err := s.db.Select(&h.History, "SELECT rank, day FROM ranking WHERE name=? ORDER BY day", domain)
+	return h, err
+}
+
+// GetHistoryHandler returns the current details for a given domain
+func (s *Server) GetHistoryHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		s.log.WithField("func", "GetHistoryHandler")
+		s.log.Infoln(r.Method, r.URL.Path, r.RemoteAddr)
+
+		vars := mux.Vars(r)
+		domain := vars["domain"]
+
+		historyRes, err := s.GetHistory(domain)
+		if err != nil {
+			s.log.Errorln(err)
+			httpResponse(w, &errorResponse{Error: "Domain not found"}, http.StatusNotFound)
+			return
+		}
+		httpResponse(w, &historyRes, http.StatusOK)
+	})
+}
+
 // Rank represents a given rank in the database
 type Rank struct {
 	Domain string `json:"domain"`
 	Rank   int    `json:"rank"`
+}
+
+// GetRank returns the details of the domain at the given rank
+func (s *Server) GetRank(rank int) (Rank, error) {
+	var r Rank
+	err := s.db.Get(&r, "SELECT domain, rank FROM current WHERE rank=?", rank)
+	return r, err
 }
 
 // GetRankHandler returns the domain at the given rank
@@ -52,8 +103,8 @@ func (s *Server) GetRankHandler() http.HandlerFunc {
 			return
 		}
 
-		var rankRes Rank
-		if err = s.db.Get(&rankRes, "SELECT domain, rank FROM current WHERE rank=?", rank); err != nil {
+		rankRes, err := s.GetRank(rank)
+		if err != nil {
 			s.log.Errorln(err)
 			httpResponse(w, &errorResponse{Error: "Rank not found"}, http.StatusNotFound)
 			return
